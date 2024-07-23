94b34d4785fef286c65e52bfc2b0da7637307ab6
Stephanie Maru <166640506+spdamazo@users.noreply.github.com>
2024-07-23 16:39:42 -0600

    update studio details

--- js (DO NOT USE)/studio-details.js
+++ js (DO NOT USE)/studio-details.js
@@ -12,11 +12,11 @@ document.addEventListener('DOMContentLoaded', () => {
             <p><strong>Address:</strong> ${studio.address}</p>
             <p><strong>Area:</strong> ${studio.area} square meters</p>
             <p><strong>Type:</strong> ${studio.type}</p>
-            <p><strong>Accommodates:</strong> ${studio.capacity}</p>
+            <p><strong>Accommodates:</strong> ${studio.accommodates}</p>
             <p><strong>Parking:</strong> ${studio.parking ? 'Yes' : 'No'}</p>
-            <p><strong>Public Transport:</strong> ${studio.public_transport ? 'Yes' : 'No'}</p>
+            <p><strong>Public Transport:</strong> ${studio.publicTransport ? 'Yes' : 'No'}</p>
             <p><strong>Available:</strong> ${studio.available ? 'Yes' : 'No'}</p>
-            <p><strong>Rental Term:</strong> ${studio.rental_term}</p>
+            <p><strong>Rental Term:</strong> ${studio.rentalTerm}</p>
             <p><strong>Price per Rental Term:</strong> ${studio.price}</p>
             <p><strong>Owner:</strong> ${studio.ownerName}</p>
             <p><strong>Owner Phone:</strong> ${studio.ownerPhone}</p>
