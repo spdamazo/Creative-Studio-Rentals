d88de5dde82a715829396a09697f31408e1d3f06
aaronamoso <amosoaaron@gmail.com>
2024-07-23 02:19:25 -0600

    added more html inputs + registration validation

---
+++ JavaScript/register(registrationForm).js
@@ -0,0 +1,20 @@
+//save element references by its unique IDs
+const form = document.getElementById('registrationForm');
+const name = document.getElementById('name');
+const phone = document.getElementById('phone');
+const email = document.getElementById('email');
+const password = document.getElementById('password');
+
+form.addEventListener('submit', e => {
+    e.preventDefault(); // prevent form from submitting because we need to validate input first
+    validateInputs();
+});
+
+// validation function, need to get the value of all input field
+
+const validateInputs = () => {
+    const nameValue = name.value.trim(); //using trim() method will REMOVE whitespaces
+    const phoneValue = phone.value.trim();
+    const emailValue = email.value.trim();
+    const passwordValue = password.value.trim();
+}
