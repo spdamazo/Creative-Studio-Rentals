b879e516ba70b5224b6e4361c4b0a6b1f0a21e0c
HyunjungJeinLim <166640615+HyunjungJeinLim@users.noreply.github.com>
2024-08-11 04:01:28 -0600

    server.js edit

--- server.js
+++ server.js
@@ -4,3 +4,19 @@ var cors = require('cors');
 const apiRoutes = require('./MVC/routes/apiRoutes');
 const app = express();
 const port = 7777;
+
+console.log("Starting server setup...");
+
+app.use(cors());
+app.use(express.static(path.join(__dirname)));
+
+console.log("CORS middleware applied.");
+console.log("Static files served from: ", path.join(__dirname));
+
+app.use('/api', apiRoutes);
+
+console.log("API routes configured.");
+
+app.listen(port, () => {
+  console.log(`Server running at http://localhost:${port}`);
+});
